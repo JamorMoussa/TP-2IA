002e5e26356f78420b5d4091452ce2a2cdfd7359
Moussa Jamor <moussajamorsup@gmail.com>
2023-09-20 22:30:55 +0100

    fix something

--- TP1/src/Exo4/TortoiseThread.java
+++ TP1/src/Exo4/TortoiseThread.java
@@ -5,14 +5,15 @@ public class TortoiseThread extends Thread{
 	@Override
 	public void run() {
 		System.out.println("The tortoise starts the race..");
-        for (int i = 0; i < 5; i++) {
-            System.out.println("La tortue se déplace lentement...");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-            	System.out.println(e);
-            }
-        }
+		try {
+			Thread.sleep(10);
+	        for (int i = 0; i < 5; i++) {
+	            	System.out.println("La tortue se déplace lentement...");
+	                Thread.sleep(1000);
+	            }
+	 	} catch (InterruptedException e) {
+	 		System.out.println(e);
+	 	}
         System.out.println("The turtle won the race !");
 	}
 }
